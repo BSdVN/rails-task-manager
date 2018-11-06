7ade408d30587589d8da66c6266d92f992f517ca
Beltran Sainz de Vicuna Nowack <gorthor@gmail.com>
2018-11-06 16:09:20 +0100

    finish challenge - style and delete function are incomplete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,38 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Tasks.all
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # redirect_to task_path(@task)
+  end
+
+  def task_params
+    params.required(:task).permit(:title, :details)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
   end
 end
